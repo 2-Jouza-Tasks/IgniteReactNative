70cd1b3485518988eb257abf98d0fce09c0d38ab
Mohammad Jouza <m.jozaa@yahoo.com>
2023-10-30 15:15:16 +0300

    style 2

--- project2/components/QuestionView .tsx
+++ project2/components/QuestionView .tsx
@@ -29,68 +29,92 @@ interface QuestionViewProps {
 const QuestionView: React.FC<QuestionViewProps> = ({ question }) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.playlistText}>{question.playlist}</Text>
-      <Text style={styles.descriptionText}>{question.description}</Text>
-      <Image source={{ uri: question.image }} style={styles.image} />
-      <Text style={styles.questionText}>{question.question}</Text>
-      <View style={styles.optionsContainer}>
-        {question.options.map((option) => (
-          <TouchableOpacity key={option.id} style={styles.option}>
-            <Text style={styles.optionText}>{option.answer}</Text>
-          </TouchableOpacity>
-        ))}
+      <Image source={{ uri: question.image }} style={styles.backgroundImage} />
+      <View style={styles.contentContainer}>
+        <View style={styles.questionContainer}>
+          <Text style={styles.questionText}>{question.question}</Text>
+        </View>
+        <View style={styles.optionsContainer}>
+          {question.options.map((option) => (
+            <TouchableOpacity key={option.id} style={styles.option}>
+              <Text style={styles.optionText}>{option.answer}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+        <View style={styles.userDescriptionContainer}>
+          <View style={styles.userInfoContainer}>
+            <Image source={{ uri: question.user.avatar }} style={styles.avatar} />
+            <Text style={styles.userText}>{question.user.name}</Text>
+          </View>
+          <Text style={styles.descriptionText}>{question.description}</Text>
+          <Text style={styles.playlistText}>{question.playlist}</Text>
+        </View>
       </View>
-      <Text style={styles.userText}>Question by: {question.user.name}</Text>
-      <Image source={{ uri: question.user.avatar }} style={styles.avatar} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  backgroundImage: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    resizeMode: 'cover',
+  },
+  contentContainer: {
+    flex: 1,
     padding: 16,
+    justifyContent: 'space-between',
   },
-  playlistText: {
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-  descriptionText: {
-    fontSize: 14,
-    color: "gray",
-  },
-  image: {
-    width: "100%",
-    height: 200,
-    resizeMode: "cover",
-    marginBottom: 10,
+  questionContainer: {
+    marginBottom: 16,
   },
   questionText: {
-    fontSize: 18,
-    fontWeight: "bold",
+    fontSize: 20,
+    fontWeight: 'bold',
   },
   optionsContainer: {
-    marginTop: 10,
+    marginBottom: 16,
   },
   option: {
     padding: 8,
     borderWidth: 1,
-    borderColor: "lightgray",
+    borderColor: 'lightgray',
     borderRadius: 8,
     marginBottom: 8,
   },
   optionText: {
     fontSize: 16,
   },
-  userText: {
-    marginTop: 20,
-    fontSize: 14,
-    color: "gray",
+  userDescriptionContainer: {},
+  userInfoContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 8,
   },
   avatar: {
     width: 40,
     height: 40,
     borderRadius: 20,
-    marginTop: 10,
+    marginRight: 8,
+  },
+  userText: {
+    fontSize: 14,
+    color: 'gray',
+  },
+  descriptionText: {
+    fontSize: 14,
+    color: 'gray',
+  },
+  playlistText: {
+    fontSize: 14,
+    fontWeight: 'bold',
   },
 });
 
