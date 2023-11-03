db551a626cb6c15656ac3a32a3375b507d08fe4c
Mohammad Jouza <m.jozaa@yahoo.com>
2023-11-04 02:40:02 +0300

    return tothe ild way

--- project2/App.tsx
+++ project2/App.tsx
@@ -3,60 +3,53 @@ import { View, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import FA5 from "react-native-vector-icons/FontAwesome5";
-import HomeScreen from "./screens/HomeScreen";
+
 import TemplateScreen from "./screens/TemplateScreen";
+import ForYou from "./components/ForYou";
 
 const Tab = createBottomTabNavigator();
 
 const App = () => {
   const staticScreens = [
-    { title: "Home", icon: "compass", component: HomeScreen },
-    { title: "Discover", icon: "compass", component: TemplateScreen },
-    { title: "Activity", icon: "stopwatch", component: TemplateScreen },
-    { title: "Bookmarks", icon: "bookmark", component: TemplateScreen },
-    { title: "Profile", icon: "user", component: TemplateScreen },
+    { title: "Discover", icon: "compass" },
+    { title: "Activity", icon: "stopwatch" },
+    { title: "Bookmarks", icon: "bookmark" },
+    { title: "Profile", icon: "user" },
   ];
-
-  const tabBarIconFunc = (
-    focused: boolean,
-    color: string,
-    name: string,
-    icon: string,
-    size: number = 30
-  ) => <FA5 name={icon} color={focused ? "white" : "gray"} size={size} />;
 
   return (
     <View style={styles.container}>
       <NavigationContainer>
         <Tab.Navigator
           screenOptions={({ route }) => ({
-            headerShown: false,
-            tabBarActiveTintColor: "white",
-            tabBarInactiveTintColor: "red",
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+
+              if (route.name === "Home") {
+                iconName = focused
+                  ? "ios-information-circle"
+                  : "ios-information-circle-outline";
+              } else if (route.name === "Settings") {
+                iconName = focused ? "ios-list" : "ios-list-outline";
+              }
+
+              // You can return any component that you like here!
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: "tomato",
+            tabBarInactiveTintColor: "gray",
           })}
         >
           <Tab.Screen
             name="Home"
             component={ForYou}
             options={{
-              tabBarIcon: (focused) => tabBarIconFunc(),
+              tabBarIcon: () => <FA5 name="home" color={"red"} size={30} />,
+              headerShown: false,
             }}
           />
-        </Tab.Navigator>
-      </NavigationContainer>
-    </View>
-  );
-};
 
-export default App;
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-/*
-
-   {staticScreens.map(({ title, icon }, index) => (
+          {staticScreens.map(({ title, icon }, index) => (
             <Tab.Screen
               name={title}
               key={`${index}.${title}`}
@@ -76,5 +69,15 @@ const styles = StyleSheet.create({
               )}
             </Tab.Screen>
           ))}
+        </Tab.Navigator>
+      </NavigationContainer>
+    </View>
+  );
+};
 
-          */
+export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
